05e4ecd24784c7f7c418c1c682e280e1ea8315fa
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2025-02-04 16:00:58 +0100

    refactor: add more questions to quiz fixture

--- src/DataFixtures/QuizFixtures.php
+++ src/DataFixtures/QuizFixtures.php
@@ -35,6 +35,7 @@ class QuizFixtures extends Fixture
         $quiz1->setUser($user);
         $quiz1->setPrompt('First Quiz Prompt');
 
+// Question 1
         $question1 = new Question();
         $question1->setTitle('First Question Title');
         $question1->setContent('First Question Content');
@@ -52,10 +53,94 @@ class QuizFixtures extends Fixture
 
         $quiz1->addQuestion($question1);
 
+// Question 2
+        $question2 = new Question();
+        $question2->setTitle('Second Question Title');
+        $question2->setContent('Second Question Content');
+
+        $option3 = new Option();
+        $option3->setContent('Third Option Content');
+        $option3->setCorrect(false);
+
+        $option4 = new Option();
+        $option4->setContent('Fourth Option Content');
+        $option4->setCorrect(true);
+
+        $question2->addOption($option3);
+        $question2->addOption($option4);
+
+        $quiz1->addQuestion($question2);
+
+// Question 3
+        $question3 = new Question();
+        $question3->setTitle('Third Question Title');
+        $question3->setContent('Third Question Content');
+
+        $option5 = new Option();
+        $option5->setContent('Fifth Option Content');
+        $option5->setCorrect(true);
+
+        $option6 = new Option();
+        $option6->setContent('Sixth Option Content');
+        $option6->setCorrect(false);
+
+        $question3->addOption($option5);
+        $question3->addOption($option6);
+
+        $quiz1->addQuestion($question3);
+
+// Question 4
+        $question4 = new Question();
+        $question4->setTitle('Fourth Question Title');
+        $question4->setContent('Fourth Question Content');
+
+        $option7 = new Option();
+        $option7->setContent('Seventh Option Content');
+        $option7->setCorrect(false);
+
+        $option8 = new Option();
+        $option8->setContent('Eighth Option Content');
+        $option8->setCorrect(true);
+
+        $question4->addOption($option7);
+        $question4->addOption($option8);
+
+        $quiz1->addQuestion($question4);
+
+// Question 5
+        $question5 = new Question();
+        $question5->setTitle('Fifth Question Title');
+        $question5->setContent('Fifth Question Content');
+
+        $option9 = new Option();
+        $option9->setContent('Ninth Option Content');
+        $option9->setCorrect(true);
+
+        $option10 = new Option();
+        $option10->setContent('Tenth Option Content');
+        $option10->setCorrect(false);
+
+        $question5->addOption($option9);
+        $question5->addOption($option10);
+
+        $quiz1->addQuestion($question5);
+
         $manager->persist($quiz1);
         $manager->persist($question1);
         $manager->persist($option1);
         $manager->persist($option2);
+        $manager->persist($question2);
+        $manager->persist($option3);
+        $manager->persist($option4);
+        $manager->persist($question3);
+        $manager->persist($option5);
+        $manager->persist($option6);
+        $manager->persist($question4);
+        $manager->persist($option7);
+        $manager->persist($option8);
+        $manager->persist($question5);
+        $manager->persist($option9);
+        $manager->persist($option10);
 
         // Second Quiz
         $quiz2 = new Quiz();
