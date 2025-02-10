37c4db1bb6031eb0f15564f694fddcc77cac88cb
YehorKuzmenko <yehor.kuzmenko@etu.univ-amu.fr>
2025-02-10 12:18:15 +0100

    feat: add translation to home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,12 +7,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, Request $request ): Response
     {
+        $locale = $request->get('_locale', $request->getDefaultLocale());
+        $request->getSession()->set('_locale', $locale);
+        $request->setLocale($locale);
+
         $quizzes = $entityManager->getRepository(Quiz::class)->findAll();
 
         return $this->render('home/index.html.twig', [
